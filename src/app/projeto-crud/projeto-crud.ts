be7b6c8abac9db48e9a319c926bd72784f53d907
Daniel <danieel.ios90@gmail.com>
2025-11-04 14:12:48 -0300

    validação de formulário

--- src/app/projeto-crud/projeto-crud.ts
+++ src/app/projeto-crud/projeto-crud.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-projeto-crud',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './projeto-crud.html',
   styleUrl: './projeto-crud.css',
 })
 export class ProjetoCrud {
 
+  //Variavel para trabalhar com  a visibilidad dos botões
+  btnCadastrar:boolean = true;
+
+  //Objeto pessoa do tipo FormGroup
+  pessoa = new FormGroup({
+    nome: new FormControl('',[Validators.required,Validators.minLength(3)]),
+    idade: new FormControl('',[Validators.required,Validators.min(0),Validators.max(100)]),
+    cidade: new FormControl('',[Validators.required,Validators.minLength(3)])
+  });
 }
