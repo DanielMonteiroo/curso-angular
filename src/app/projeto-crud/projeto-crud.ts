84bcc57c4237516983932a1984527fbce97efb2d
Daniel <danieel.ios90@gmail.com>
2025-11-04 14:42:30 -0300

    cadastro e listagem

--- src/app/projeto-crud/projeto-crud.ts
+++ src/app/projeto-crud/projeto-crud.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../Modelo/Pessoa';
 
 @Component({
   selector: 'app-projeto-crud',
@@ -18,4 +19,14 @@ export class ProjetoCrud {
     idade: new FormControl('',[Validators.required,Validators.min(0),Validators.max(100)]),
     cidade: new FormControl('',[Validators.required,Validators.minLength(3)])
   });
+
+  //Vetor para armazenamento de pessoas cadastradas
+  vetor: Pessoa[] = [];
+
+  //Método para cadastrar
+  cadastrar():void {
+    this.vetor.push(this.pessoa.value as Pessoa); //as Pessoa faz referencia a classe criada na pasta Modelo
+    this.pessoa.reset(); //limpar o formulario apos o cadastro
+  }
+
 }
