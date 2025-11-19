7f09d46a7e5c1ec32dec123006644598069efb6d
Daniel <danieel.ios90@gmail.com>
2025-11-19 10:52:18 -0300

    lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,9 @@ import { Produto } from './paginas/produto/produto';
 
 export const routes: Routes = [
   {path:'inicio',component:Inicio},
-  {path:'sobre',component:Sobre},
-  {path:'contato',component:Contato},
-  {path:'produto/:codigo',component:Produto},
+  {path:'sobre',loadComponent: ()=>import('./paginas/sobre/sobre').then(c => c.Sobre)},
+  {path:'contato',loadComponent: ()=>import('./paginas/contato/contato').then(c => c.Contato)},
+  {path:'produto/:codigo',loadComponent: ()=>import('./paginas/produto/produto').then(c => c.Produto)},
   {path:'',redirectTo:'inicio',pathMatch:'full'}, //redirecionamento para a pag inicial
-  {path:'**',component:NaoEncontrado} //exibição de mensagem de pagina nao encontrada
+  {path:'**',loadComponent: ()=>import('./paginas/nao-encontrado/nao-encontrado').then(c => c.NaoEncontrado)} //exibição de mensagem de pagina nao encontrada
 ];
