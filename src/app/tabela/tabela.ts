f390a902882972dde71fdfb009b6a0c01652ec02
Daniel <danieel.ios90@gmail.com>
2025-12-02 11:54:50 -0300

    listagem de dados na tabela com @Input

--- src/app/tabela/tabela.ts
+++ src/app/tabela/tabela.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pessoa } from '../modelo/Pessoa';
 
 @Component({
   selector: 'app-tabela',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './tabela.css',
 })
 export class Tabela {
+  @Input() vetor:Pessoa[] =[];
 
 }
